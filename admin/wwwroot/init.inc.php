3fa7ca44e3d4907c18a5f7a3a00e4f94fab6efe8
lonny <xingchenyekong@163.com>
2017-10-11 17:50:12 +0800

    change

--- admin/wwwroot/init.inc.php
+++ admin/wwwroot/init.inc.php
@@ -76,7 +76,7 @@ if (!ENV_PRODUCTION) {
 !defined('DB_HOST') && define('DB_HOST', '127.0.0.1');
 !defined('DB_PORT') && define('DB_PORT', 3306);
 !defined('DB_USERNAME') && define('DB_USERNAME', 'root');
-!defined('DB_PASSWORD') && define('DB_PASSWORD', 'root');
+!defined('DB_PASSWORD') && define('DB_PASSWORD', '9998');
 !defined('DB_DATABASE') && define('DB_DATABASE', 'db_bl');
 
 !defined('DB2_HOST') && define('DB2_HOST', '');
