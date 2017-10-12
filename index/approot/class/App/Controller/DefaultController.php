24ae5cb8bab076e7a475c4665b2361cb8a88a8c2
lonny <xingchenyekong@163.com>
2017-10-12 16:50:13 +0800

    change

--- index/approot/class/App/Controller/DefaultController.php
+++ index/approot/class/App/Controller/DefaultController.php
@@ -41,6 +41,9 @@ class DefaultController extends AbstractActionController
             foreach ($recommendList as $key => $row) {
                 if ($row['product_code']) {
                     $recommendList[$key]['product_id'] = $this->models->product->getProductId(sprintf("product_code = '%s'", $row['product_code']));
+
+                    $sql = "SELECT product_type FROM t_products WHERE product_code = ?";
+                    $recommendList[$key]['product_type'] = $this->locator->db->getOne($$sql, $row['product_code']);
                 } else {
                     $recommendList[$key]['product_id'] = '';
                 }
