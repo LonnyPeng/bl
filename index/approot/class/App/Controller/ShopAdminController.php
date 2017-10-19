18b1c04e8331a2801f8a42881b08b5384ed86bcc
lonny <xingchenyekong@163.com>
2017-10-19 12:08:04 +0800

    change

--- index/approot/class/App/Controller/ShopAdminController.php
+++ index/approot/class/App/Controller/ShopAdminController.php
@@ -56,19 +56,89 @@ class ShopAdminController extends AbstractActionController
 
     public function selfAction()
     {
-        require_once VENDOR_DIR . 'autoload.php';
+        if (!$this->funcs->isAjax()) {
+            $this->funcs->redirect($this->helpers->url('default/index'));
+        }
 
-        $this->app = new Application(require_once CONFIG_DIR . 'wechat.config.php');
+        $key = trim($_POST['key']);
+        $orderNumber = $this->funcs->encrypt($key, 'D', QRCODE_KEY);
+        $info = $this->models->order->getOrderByNumber($orderNumber);
 
-        $this->layout->title = '扫一扫';
+        if (!$info) {
+            return new JsonModel('error', '订单不存在');
+        }
+        if ($info['order_type'] != 'pending') {
+            return new JsonModel('error', '商品已派送');
+        }
+        if ($info['shinging_type'] != 'self') {
+            return new JsonModel('error', '商品非自提');
+        }
 
-        // $orderNumber = trim($this->param('key'));
-        // $info = $this->models->order->getOrderByNumber($orderNumber);
+        //获取订单商品
+        $product = $this->models->product->getProductById($info['product_id']);
+        if (!$product) {
+            return new JsonModel('error', '商品不存在');
+        }
+        $time = strtotime($info['order_time']) + $product['product_qr_code_day'] * 86400 - time();
+        if ($time < 0) {
+            return new JsonModel('error', '二维码已过期');
+        }
 
-        // print_r($info);die;
-        return array(
-            'js' => $this->app->js,
+        $sql = "SELECT quantity_num 
+                FROM t_product_quantity 
+                WHERE product_id = ? 
+                AND shop_id = ?";
+        if ($this->locator->db->getOne($sql, $info['product_id'], $this->shopInfo['shop_id']) < 1) {
+            return new JsonModel('error', '库存不足');
+        }
+
+        return JsonModel::init('ok', '', array(
+            'href' => (string) $this->helpers->url('shop/self-edit', array('id' => $info['order_id'])),
+            'name' => $info['product_name'],
+            'src' => (string) $this->helpers->uploadUrl($product['images']['banner'][0]['image_path'], 'product'),
+        ));
+    }
+
+    public function selfEditAction()
+    {
+        if (!$this->funcs->isAjax()) {
+            $this->funcs->redirect($this->helpers->url('default/index'));
+        }
+
+        $id = trim($this->param('id'));
+        $where = array(
+            sprintf("order_id = %d", $id),
+            "order_type = 'pending'",
+            "shinging_type = 'self'",
+            "order_status = 1",
         );
+        $info = $this->models->order->getOrderInfo($where);
+        if (!$info) {
+            return new JsonModel('error', '订单不存在，无法派送');
+        }
+
+        $sql = "UPDATE t_orders 
+                SET order_type = 'shipped', 
+                order_shipped_time = now(), 
+                shop_id = :shop_id, 
+                user_address = :user_address 
+                WHERE order_id = ?";
+        $status = $this->locator->db->exec($sql, array(
+            'shop_id' => $this->shopInfo['shop_id'],
+            'user_address' => $this->shopInfo['shop_address'],
+        ));
+        if (!$status) {
+            return new JsonModel('error', '派送失败');
+        }
+
+        //修改库存
+        $sql = "UPDATE t_product_quantity 
+                SET quantity_num = quantity_num - 1 
+                WHERE product_id = ? 
+                AND shop_id = ?";
+        $this->locator->db->exec($sql, $info['product_id'], $this->shopInfo['shop_id']);
+
+        return new JsonModel('error', '派送成功');
     }
 
     public function logoutAction()
