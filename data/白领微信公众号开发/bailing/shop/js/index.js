3aa890d193543a224c4b419e371a1d3073ee6cd0
lonny <xingchenyekong@163.com>
2017-10-27 17:11:37 +0800

    change

--- data/白领微信公众号开发/bailing/shop/js/index.js
+++ data/白领微信公众号开发/bailing/shop/js/index.js
@@ -1,5 +1,17 @@
 $(function(){
 	var windowH=$(window).height();
 	$(".slogin").height(windowH);
+	$(".login_btn").click(function(){
+		if(!$(".pass_inp").val()){
+			$.alert({
+				  title: '提示',
+				  text: '请输入密码',
+				  onOK: function () {
+				    //点击确认
+				    $(".pass_inp").focus()
+				  }
+				});
+		}
+	})
 })
 
