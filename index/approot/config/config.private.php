f1e3cbc910acc9b62815a73ca67c7d96c4457825
lonny <xingchenyekong@163.com>
2017-10-30 11:11:08 +0800

    init

--- index/approot/config/config.private.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-!defined('DB_HOST') && define('DB_HOST', '127.0.0.1');
-!defined('DB_PORT') && define('DB_PORT', 3306);
-!defined('DB_USERNAME') && define('DB_USERNAME', 'root');
-!defined('DB_PASSWORD') && define('DB_PASSWORD', 'root');
-!defined('DB_DATABASE') && define('DB_DATABASE', 'db_bl');
-
