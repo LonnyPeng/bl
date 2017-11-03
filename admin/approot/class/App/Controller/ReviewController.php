f9e87a3172d581cea0d01675c03c28ebadfcc28e
lonny <xingchenyekong@163.com>
2017-11-03 17:53:38 +0800

    change

--- admin/approot/class/App/Controller/ReviewController.php
+++ admin/approot/class/App/Controller/ReviewController.php
@@ -23,11 +23,62 @@ class ReviewController extends AbstractActionController
 		$this->perm->check(PERM_READ);
 		
 		$where = array();
-		if ($this->param('customer_name')) {
-			$where[] = sprintf("p.customer_name LIKE '%s'", addslashes('%' . $this->helpers->escape(trim($this->param('customer_name'))) . '%'));
-		}
 		if ($this->param('product_name')) {
 			$where[] = sprintf("p.product_name LIKE '%s'", addslashes('%' . $this->helpers->escape(trim($this->param('product_name'))) . '%'));
+		}
+		if ($this->param('product_code')) {
+			$where[] = sprintf("p.product_code = '%s'", trim($this->param('product_code')));
+		}
+
+		$count = $this->models->product->getCount(array('setWhere' => $where));
+		$this->helpers->paginator($count, 10);
+		$limit = array($this->helpers->paginator->getLimitStart(), $this->helpers->paginator->getItemCountPerPage());
+
+		$files = array(
+			"*",
+			"(SELECT COUNT(*) FROM t_reviews r WHERE r.product_id = p.product_id) AS reviewCount",
+			"(SELECT AVG(r.review_score) FROM t_reviews r WHERE r.product_id = p.product_id) AS reviewAve",
+			"(SELECT COUNT(*) FROM t_reviews r WHERE r.product_id = p.product_id AND r.review_score >= 1 AND r.review_score < 2) AS reviewCount1",
+			"(SELECT COUNT(*) FROM t_reviews r WHERE r.product_id = p.product_id AND r.review_score >= 2 AND r.review_score < 3) AS reviewCount2",
+			"(SELECT COUNT(*) FROM t_reviews r WHERE r.product_id = p.product_id AND r.review_score >= 3 AND r.review_score < 4) AS reviewCount3",
+			"(SELECT COUNT(*) FROM t_reviews r WHERE r.product_id = p.product_id AND r.review_score >= 4 AND r.review_score < 5) AS reviewCount4",
+			"(SELECT COUNT(*) FROM t_reviews r WHERE r.product_id = p.product_id AND r.review_score >= 5) AS reviewCount5",
+		);
+
+		$join = 'LEFT JOIN t_product_attr pa ON pa.attr_id = p.attr_id';
+		$sqlInfo = array(
+			'setJoin' => $join,
+			'setWhere' => $where,
+			'setLimit' => $limit,
+			'setOrderBy' => 'reviewCount DESC, product_id DESC',
+		);
+
+		$productList = $this->models->product->getProduct($files, $sqlInfo);
+		if ($productList) {
+			foreach ($productList as $key => $row) {
+				$productList[$key]['reviewAveStar'] = $this->getStarNum($row['reviewAve']);
+			}
+		}
+
+		// print_r($productList);die;
+		return array(
+			'productList' => $productList,
+		);
+	}
+
+	public function rowAction()
+	{
+		$this->perm->check(PERM_READ);
+
+		$id = trim($this->param('id'));
+		$info = $this->models->product->getProductById($id);
+		if (!$info) {
+			$this->funcs->redirect($this->helpers->url('review/list'));
+		}
+
+		$where = array(sprintf("r.product_id = %d", $id));
+		if ($this->param('customer_name')) {
+			$where[] = sprintf("p.customer_name LIKE '%s'", addslashes('%' . $this->helpers->escape(trim($this->param('customer_name'))) . '%'));
 		}
 		if ($this->param('review_attr')) {
 			$where[] = sprintf("review_attr = '%s'", trim($this->param('review_attr')));
@@ -35,20 +86,19 @@ class ReviewController extends AbstractActionController
 
 		$join = array(
 			'LEFT JOIN t_customers c ON c.customer_id = r.customer_id',
-			'LEFT JOIN t_products p ON p.product_id = r.product_id',
 		);
 
 		$count = $this->models->review->getCount(array('setWhere' => $where, 'setJoin' => $join));
 		$this->helpers->paginator($count, 10);
 		$limit = array($this->helpers->paginator->getLimitStart(), $this->helpers->paginator->getItemCountPerPage());
 
-		$files = array('r.*', 'c.customer_name', 'p.product_name');
+		$files = array('r.*', 'c.customer_name');
 
 		$sqlInfo = array(
 			'setJoin' => $join,
 			'setWhere' => $where,
 			'setLimit' => $limit,
-			'setOrderBy' => 'product_id DESC',
+			'setOrderBy' => 'r.review_score DESC, r.review_id DESC',
 		);
 
 		$reviewList = $this->models->review->getReview($files, $sqlInfo);
