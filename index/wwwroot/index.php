3afa627504ccf112c6b8428416e91201955be3c0
lonny <xingchenyekong@163.com>
2017-11-13 15:55:52 +0800

    change

--- index/wwwroot/index.php
+++ index/wwwroot/index.php
@@ -1,28 +1,5 @@
 <?php
 
-//微信认证
-define("TOKEN", "bailin");
-if (isset($_GET["signature"]) && isset($_GET["timestamp"]) 
-	&& isset($_GET["nonce"]) && isset($_GET["echostr"])) {
-	$signature = $_GET["signature"];
-	$timestamp = $_GET["timestamp"];
-	$nonce = $_GET["nonce"];
-	$token = TOKEN;
-
-	$tmpArr = array($token, $timestamp, $nonce);
-	sort($tmpArr, SORT_STRING);
-
-	$tmpStr = implode($tmpArr);
-	$tmpStr = sha1($tmpStr);
-
-	if($tmpStr == $signature){
-	    echo $_GET["echostr"];
-	} else {
-	    echo 'error';
-	}
-}
-die;
-
 require_once 'init.inc.php';
 
 session_start();
